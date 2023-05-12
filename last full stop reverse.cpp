94c3fb2e52abde1a5ef21bde32ab518590d838f6
Tejas Lambat <133117021+tejaslambat03@users.noreply.github.com>
2023-05-12 13:18:56 +0530

    Add files via upload

---
+++ last full stop reverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=10;i>=1;i--)
+    {
+    if (i>1)
+    cout<<i<<",";
+    else
+    cout<<i<<".";
+    }
+    
+    
+}
