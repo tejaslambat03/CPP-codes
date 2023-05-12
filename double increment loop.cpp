b806bb8de471277b69a97f3146afa85e498b16ae
Tejas Lambat <133117021+tejaslambat03@users.noreply.github.com>
2023-05-12 13:05:28 +0530

    Add files via upload

---
+++ double increment loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=2;i<=20;i=i+2)
+    {
+    if (i<20)
+    cout<<i<<",";
+    else
+    cout<<i<<".";
+    }
+    
+    
+}
